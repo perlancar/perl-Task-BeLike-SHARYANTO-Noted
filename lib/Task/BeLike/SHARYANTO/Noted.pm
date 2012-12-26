d72994834936fe0c8b2dacdbc08c864aaf2df62d
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-12-26 17:23:31 +0700

    Initial commit

---
+++ lib/Task/BeLike/SHARYANTO/Noted.pm
@@ -0,0 +1,23 @@
+package Task::BeLike::SHARYANTO::Noted;
+
+# VERSION
+
+1;
+# ABSTRACT: Some modules which I take note of because I can never remember their names
+
+=head1 DESCRIPTION
+
+This distribution is a random list of modules which I need to take a note of
+because I can never seem to remember their names, usually because the name is
+not representative of what the module does, or the term is unusual, or both. Or
+it's just my failing memory.
+
+They are categorized by functionalities/topics.
+
+=pkgroup Modules to prevent multiple instances of script running simultaneously
+
+Similar in functionality to L<Proc::PID::File>.
+
+=pkg Script::RunAlone
+
+=cut
